cd311773efc9e483582f4ada465aaaa81352e270
t3jas12 <tejaspq2@gmail.com>
2026-04-26 14:35:37 +0530

    deployment fixes v3

--- server/index.js
+++ server/index.js
@@ -67,17 +67,6 @@ app.use((err, req, res, next) => {
   })
 })
 
-const path = require('path');
-
-// --- PRODUCTION DEPLOYMENT SETUP ---
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
-  });
-}
-
 // server entry
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
