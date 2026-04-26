1cbbf4db1ec22378b4614ee0f997fb5f59a74757
t3jas12 <tejaspq2@gmail.com>
2026-04-26 14:33:01 +0530

    deployment fixes v2

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.json())
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: process.env.VITE_URL,
+    origin: process.env.ORIGIN_CLIENT,
     credentials: true
 }));
 
