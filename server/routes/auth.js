0f31e0d678688b6aa0a3d106c7e5397b8aa36929
t3jas12 <tejaspq2@gmail.com>
2026-04-26 14:43:07 +0530

    deployment fixes v4

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -68,7 +68,7 @@ authRouter.post("/login", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 1 * 24 * 60 * 60 * 1000
     });
 
